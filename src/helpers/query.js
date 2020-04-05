50ce16bf0e9136e28d9db31981f7880d5172a2b2
manhdinh141 <manhdinh141@gmail.com>
2020-04-05 20:34:10 +0700

    Resource Class: add setId(), setType, setAttributes(), setAttribute(), setRelationship()

--- src/helpers/query.js
+++ src/helpers/query.js
@@ -1,117 +1,100 @@
 class Query {
+  /**
+   * Check resource match query data
+   *
+   * @param array
+   * @param Resource
+   * @return bool
+   */
+  checkResource(query, resource) {
+    // If operation was not set
+    if (query.length === 2) {
+      // Set default operator as =
+      query = [query[0], "=", query[1]];
+    }
 
-	/**
-	* Check resource match query data
-	*
-	* @param array
-	* @param Resource
-	* @return bool
-	*/
-	checkResource(query, resource) {
-
-		// If operation was not set
-		if (query.length === 2) {
-
-			// Set default operator as =
-			query = [
-				query[0],
-				'=',
-				query[1]
-			];
-
-		}
-
-		let field = query[0]
-		let operator = query[1];
-		let value = query[2];
+    let field = query[0];
+    let operator = query[1];
+    let value = query[2];
 
     // If invalid operator
-    if (['=', '>', '<', '>=', '<=', 'IN'].indexOf(query[1]) === -1) {
+    if (["=", ">", "<", ">=", "<=", "IN"].indexOf(query[1]) === -1) {
       return false;
     }
 
     // Query field is attribute
-    if (['id', 'type'].indexOf(query[0]) === -1) {
-    	return this.compare(operator, resource.attributes[field], value);
-    } else { // Query field is id or type
+    if (["id", "type"].indexOf(query[0]) === -1) {
+      return this.compare(operator, resource.attributes[field], value);
+    } else {
+      // Query field is id or type
       return this.compare(operator, resource[field], value);
     }
+  }
 
-	}
+  /**
+   * Operator check
+   *
+   * @param string operator
+   * @param mixed Value 1
+   * @param mixed Value 2
+   * @param integer (Optional) Precision for float number comparing - Default 2
+   */
+  compare(operator, value1, value2, precision) {
+    // Preparation for '> < >= <=' operator
+    if ([">", "<", ">=", "<="].indexOf(operator) > -1 && !precision) {
+      precision = 2;
+      let multiplier = 1;
 
-	/**
-	* Operator check
-	*
-	* @param string operator
-	* @param mixed Value 1
-	* @param mixed Value 2
-	* @param integer (Optional) Precision for float number comparing - Default 2
-	*/
-	compare(operator, value1, value2, precision) {
+      for (let i = 1; i <= precision; i++) {
+        multiplier *= 10;
+      }
 
-		// Preparation for '> < >= <=' operator
-		if (['>', '>', '>=', '<='].indexOf(operator) > -1 && !precision) {
+      value1 = parseInt(parseFloat(value1) * multiplier);
+      value2 = parseInt(parseFloat(value2) * multiplier);
+    }
 
-			precision = 2;
-			let multiplier = 1;
+    // Preparation for 'IN' operator
+    if (operator === "IN") {
+      // If query value is not array
+      if (!Array.isArray(value2)) {
+        console.log('Query value for "IN" operator must be an array');
+        return false;
+      }
 
-			for (let i = 1; i <= precision; i++) {
-				multiplier *= 10;
-			}
+      // Force convert all element to string
+      value2 = value2.map(element => {
+        return JSON.stringify(element);
+      });
+    }
 
-			value1 = parseInt(parseFloat(value1) * multiplier);
-			value2 = parseInt(parseFloat(value2) * multiplier);
+    switch (operator) {
+      case "=":
+        return JSON.stringify(value1) === JSON.stringify(value2); // Force convert both to string and compare
+        break;
 
-		}
+      case ">":
+        return value1 > value2;
+        break;
 
-		// Preparation for 'IN' operator
-		if (operator === 'IN') {
+      case "<":
+        return value1 < value2;
+        break;
 
-			// If query value is not array
-			if (!Array.isArray(value2)) {
-				console.log('Query value for "IN" operator must be an array');
-				return false;
-			}
+      case ">=":
+        return value1 >= value2;
+        break;
 
-			// Force convert all element to string
-			value2 = value2.map(element => {
-				return JSON.stringify(element);
-			});
-			
-		} 
+      case "<=":
+        return value1 <= value2;
+        break;
 
-		switch (operator) {
+      case "IN":
+        return value2.indexOf(JSON.stringify(value1)) > -1;
+        break;
+    }
 
-			case '=':
-			return JSON.stringify(value1) === JSON.stringify(value2); // Force convert both to string and compare
-			break;
-
-			case '>':
-			return value1 > value2;
-			break;
-
-			case '<':
-			return value1 < value2;
-			break;
-
-			case '>=':
-			return value1 >= value2;
-			break;
-
-			case '<=':
-			return value1 <= value2;
-			break;
-
-			case 'IN':
-			return value2.indexOf(JSON.stringify(value1)) > -1;
-			break;
-
-		}
-
-		return false;
-
-	}
-
+    return false;
+  }
 }
 
 export default new Query();
--- src/resource.js
+++ src/resource.js
@@ -1,62 +1,103 @@
 export class Resource {
-
   constructor(data, jsonapi) {
-    this.data = data;
+    this.data = {};
+
+    // check data is valid
+    if (data) {
+      this.data = data;
+    }
+
     this.jsonapi = jsonapi;
   }
 
   /**
-  * Get resource id
-  *
-  * @return string
-  */
+   * Get resource id
+   *
+   * @return string
+   */
   getId() {
     return this.data.id;
   }
 
   /**
-  * Get resource type
-  *
-  * @return string
-  */
+   * Set resource id
+   * @param string resource id
+   * @return
+   */
+  setId(id) {
+    this.data.id = id;
+  }
+
+  /**
+   * Get resource type
+   *
+   * @return string
+   */
   getType() {
     return this.data.type;
   }
 
   /**
-  * Get resource attribute value
-  *
-  * @param string Attribute name
-  * @param mixed Default value - in case attribute not found
-  * @return mixed|null
-  */
+   * Set resource type
+   * @param string resource id
+   * @return
+   */
+  setType(type) {
+    this.data.type = type;
+  }
+
+  /**
+   * Get resource attribute value
+   *
+   * @param string Attribute name
+   * @param mixed Default value - in case attribute not found
+   * @return mixed|null
+   */
   getAttribute(attributeName, defaultValue) {
-  
     if (this.data.attributes.hasOwnProperty(attributeName)) {
       return this.data.attributes[attributeName];
     }
 
     return defaultValue || null;
-  
   }
 
   /**
-  * Get attributes
-  *
-  * @return array
-  */
+   * Set resource attribute value
+   *
+   * @param string Attribute name
+   * @param object Attribute value
+   * @return
+   */
+  setAttribute(attributeName, attributeValue) {
+    this.data.attributes[attributeName] = attributeValue;
+  }
+
+  /**
+   * Get attributes
+   *
+   * @return array
+   */
   getAttributes() {
     return this.data.attributes;
   }
 
   /**
-  * Get relationship data
-  *
-  * @param string Relationship key
-  * @return Resource|array
-  */
-  getRelationshipData(relationshipKey) {
+   * Set resource attributes value
+   *
+   * @param object Attributes value
+   * @return
+   */
+  setAttributes(attributes) {
+    this.data.attributes = attributes;
+  }
 
+  /**
+   * Get relationship data
+   *
+   * @param string Relationship key
+   * @return Resource|array
+   */
+  getRelationshipData(relationshipKey) {
     // Return null in case of no relationship found
     if (!this.data.relationships.hasOwnProperty(relationshipKey)) {
       return null;
@@ -67,42 +108,42 @@ export class Resource {
 
     // If relationship is to-many
     if (Array.isArray(relationshipData)) {
-      
       // Get ids and type
       let type = null;
       let ids = relationshipData.map(relationship => {
-
         // Set type
         if (!type) {
           type = relationship.type;
         }
 
         return relationship.id;
-
       });
 
       // Make a query
       return this.jsonapi.findResources([
-        
         // First query is type
-        [
-          'type',
-          type
-        ],
+        ["type", type],
 
         // Second query is finding by ids
-        [
-          'id',
-          'IN',
-          ids
-        ]
-
+        ["id", "IN", ids]
       ]);
-
     }
 
     // Find resource
-    return this.jsonapi.findResource(relationshipData.type, relationshipData.id);
+    return this.jsonapi.findResource(
+      relationshipData.type,
+      relationshipData.id
+    );
+  }
 
+  /**
+   * Set relationship data
+   *
+   * @param string Relationship key
+   * @param Object Relationship value
+   * @return
+   */
+  setRelationshipData(key, value) {
+    this.data.relationships[key] = value;
   }
 }
